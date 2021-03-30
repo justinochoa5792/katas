3ed0e643cf3abacfd26cc001450645876b6ecc99
Justin Ochoa <just.och92@gmail.com>
2021-03-30 19:27:24 -0400

    3 katas

---
+++ easy/Lessthan100.js
@@ -0,0 +1,10 @@
+// Less Than 100?
+// Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+function lessThan100(a, b) {
+  let total = a + b;
+  if (total < 100) {
+    return true;
+  } else {
+    return false;
+  }
+}
---
+++ easy/SumofPolygons.js
@@ -0,0 +1,3 @@
+// Sum of Polygon Angles
+// Given an n-sided regular polygon n, return the total sum of internal angles (in degrees).
+

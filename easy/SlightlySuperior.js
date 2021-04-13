f8b583264bf0d385aac4faac9009e5d7fc946dac
Justin Ochoa <just.och92@gmail.com>
2021-04-12 22:14:20 -0400

    2 katas

---
+++ easy/SlightlySuperior.js
@@ -0,0 +1,7 @@
+// Slightly Superior
+// You will be given two extremely similar arrays, but exactly one of the items in an array will be valued slightly higher than its counterpart (which means that evaluating the value > the other value will return true).
+function isFirstSuperior(arr1, arr2) {
+  if (arr1 > arr2) {
+    return true;
+  } else return false;
+}
---
+++ easy/dividesEvenly.js
@@ -0,0 +1,8 @@
+// Divides Evenly
+// Given two integers, a and b, return true if a can be divided evenly by b. Return false otherwise.
+function dividesEvenly(a, b) {
+  if (a % b === 0) {
+    return true;
+  }
+  return false;
+}

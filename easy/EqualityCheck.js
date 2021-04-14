e3b77a6e5ea08e99bdd3262762653dcf2356f244
Justin Ochoa <just.och92@gmail.com>
2021-04-13 21:14:01 -0400

    3 coding challenges

---
+++ easy/EqualityCheck.js
@@ -0,0 +1,14 @@
+// Equality Check
+// In this challenge, you must verify the equality of two different values given the parameters a and b.
+// Both the value and type of the parameters need to be equal. The possible types of the given parameters are:
+// Numbers
+// Strings
+// Booleans (false or true)
+// Special values: undefined, null and NaN
+
+function checkEquality(a, b) {
+  if (a === b) {
+    return true;
+  }
+  return false;
+}
---
+++ easy/LengthofString.js
@@ -0,0 +1,6 @@
+// Recursion: Length of a String
+// Write a function that returns the length of a string. Make your function recursive.
+
+function length(str) {
+  return str.length;
+}

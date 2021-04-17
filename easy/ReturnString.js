63cab887cd3a298d6035bc1db914ce55556bdb9d
Justin Ochoa <just.och92@gmail.com>
2021-04-16 20:09:55 -0400

    3 challenges

---
+++ easy/ReturnString.js
@@ -0,0 +1,6 @@
+// Return a String as an Integer
+// Create a function that takes a string and returns it as an integer.
+function stringInt(str) {
+  let string = parseInt(str);
+  return string;
+}

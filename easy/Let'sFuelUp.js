90f5ecda1e07255fe230a6360eab8962da1ffa18
Justin Ochoa <just.och92@gmail.com>
2021-05-04 19:47:13 -0400

    2 katas

---
+++ easy/Let'sFuelUp.js
@@ -0,0 +1,6 @@
+// Let's Fuel Up!
+// A vehicle needs 10 times the amount of fuel than the distance it travels. However, it must always carry a minimum of 100 fuel before setting off.
+// Create a function which calculates the amount of fuel it needs, given the distance
+function calculateFuel(n) {
+  return Math.max(n * 10, 100);
+}
---
+++ easy/divisibleByFive.js
@@ -0,0 +1,7 @@
+// Check if an Integer is Divisible By Five
+// Create a function that returns true if an integer is evenly divisible by 5, and false otherwise.
+function divisibleByFive(n) {
+  if (n % 5 !== 0) {
+    return false;
+  } else return true;
+}

9a71987b1b8ff3968af13f0994d40175d3bfabd5
Justin Ochoa <just.och92@gmail.com>
2021-05-15 10:15:44 -0400

    4 challenges

---
+++ easy/BuggyCode(Part 1).js
@@ -0,0 +1,12 @@
+// Buggy Code (Part 1)
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+// Original
+function cubes(a) {
+	retunr a ** 3
+}
+
+// fix
+function cubes(a) {
+	return a ** 3
+}

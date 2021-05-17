fd05e0a03ddb844bfea2c5a9b3f91cff4d6d2a20
Justin Ochoa <just.och92@gmail.com>
2021-05-17 19:43:22 -0400

    4 coding challenges

---
+++ easy/fixMistakes.js
@@ -0,0 +1,12 @@
+// Correct the Mistakes
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+// original
+function squaed(b) {
+  return a * a;
+}
+
+//  fixed
+function squared(b) {
+  return b * b;
+}

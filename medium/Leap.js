f92a09745c2009379863b4fec988c186c6e85f39
Justin Ochoa <just.och92@gmail.com>
2021-05-26 07:56:50 -0400

    3 challenges

---
+++ medium/Leap.js
@@ -0,0 +1,8 @@
+// Leap Year Function ⌚
+// Write a function that returns true if a year is a leap, otherwise return false
+
+function leapYear(year) {
+  if (year % 4) {
+    return false;
+  } else return true;
+}
---
+++ medium/ternary.js
@@ -0,0 +1,6 @@
+// Using Ternary Operators
+// The ternary operator (sometimes called Conditional Expressions) in JavaScript is an alternative to the if... else... statement
+
+function yeah_nope(bool) {
+  return bool ? "yeah" : "nope";
+}

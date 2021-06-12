37b8a246a469adee65c6302f74a5d1b24cf5050b
Justin Ochoa <just.och92@gmail.com>
2021-06-11 20:31:38 -0400

    2 challenges

---
+++ medium/EvenOdd.js
@@ -0,0 +1,8 @@
+// Is the Number Even or Odd?
+// Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers.
+
+function isEvenOrOdd(num) {
+  if (num % 2 === 0) {
+    return "even";
+  } else return "odd";
+}
---
+++ medium/ManyOperators.js
@@ -0,0 +1,5 @@
+// Many Operators!
+// Some basic arithmetic operators are +, -, *, /, and %. In this challenge you will be given three parameters, num1, num2, and an operator. Use the operator on number 1 and 2.
+function operate(num1, num2, operator) {
+  return eval(num1 + operator + num2);
+}

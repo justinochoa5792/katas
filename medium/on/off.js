b613c38e4ca176bfc32e5d5ce83d049cbbcdd0c2
Justin Ochoa <just.och92@gmail.com>
2021-06-14 08:23:12 -0400

    2 challenges

---
+++ medium/on/off.js
@@ -0,0 +1,6 @@
+// On/Off Switches
+// Create a function that returns how many possible arrangements can come from a certain number of switches (on / off). In other words, for a given number of switches, how many different patterns of on and off can we have?
+
+function posCom(num) {
+  return Math.pow(2, num);
+}

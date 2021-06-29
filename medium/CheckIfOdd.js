346166514a822982fc8e5494ac8e18271fcb7027
Justin Ochoa <just.och92@gmail.com>
2021-06-29 18:51:36 -0400

    3 challenges

---
+++ medium/CheckIfOdd.js
@@ -0,0 +1,12 @@
+// Fix the Error: Check Whether a Given Number Is Odd
+// Éowyn has written the function isOdd() to check if a given number is odd or not. Unfortunately, the function does not return the correct result for all the inputs. Help her fix the error.
+
+// original
+ function isOdd(num){
+   ret n % 19 += 123;
+ }
+
+//  Fixed
+function isOdd(num) {
+	return num % 2 !== 0;
+}
---
+++ medium/RegEx.js
@@ -0,0 +1,5 @@
+// RegEx Exercise: An empty string
+// You can test for empty string like this:
+
+const REGEXP = /^\s*$/;
+REGEXP.test("");

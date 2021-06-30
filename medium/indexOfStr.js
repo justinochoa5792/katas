a59f9f9c4974c1a77a051a5d12a89fb4eb40232b
Justin Ochoa <just.och92@gmail.com>
2021-06-30 07:29:12 -0400

    2 challanges

---
+++ medium/indexOfStr.js
@@ -0,0 +1,6 @@
+// Find the Index
+// Create a function that takes an array and a string as arguments and return the index of the string.
+
+function findIndex(arr, str) {
+  return arr.indexOf(str);
+}

bec5808781ea62375bea80390edff5f182dd3d88
Justin Ochoa <just.och92@gmail.com>
2021-08-30 19:34:26 -0400

    2 challenges

---
+++ medium/ColorInvert.js
@@ -0,0 +1,6 @@
+// Invert Colors
+// Create a function that inverts the rgb values of a given tuple.
+
+function colorInvert(rgb) {
+  return rgb.map((r) => 255 - r);
+}
---
+++ medium/destructingArr4.js
@@ -0,0 +1,6 @@
+// ES6: Destructuring Arrays IV
+// There is an easy way to assign to array values to the nth index by using the Rest element.
+
+// Change the string so that it will evaluate to head = 1 and tail = [2, 3, 4]
+// Use the Rest element
+const str = `[head, ...tail] = [1, 2, 3, 4]`;

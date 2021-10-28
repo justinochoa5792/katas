69f018962469df552362ef2bf7caacdce2728611
Justin Ochoa <just.och92@gmail.com>
2021-10-28 19:15:48 -0400

    2 challenges

---
+++ medium/oopCalculator.js
@@ -0,0 +1,22 @@
+// Simple OOP Calculator
+// Create functions for the Calculator class that can do the following:
+// Add two numbers.
+// Subtract two numbers.
+// Multiply two numbers.
+// Divide two numbers.
+
+class Calculator {
+  // Write functions to add(), subtract(), multiply() and divide()
+  add(a, b) {
+    return a + b;
+  }
+  subtract(a, b) {
+    return a - b;
+  }
+  multiply(a, b) {
+    return a * b;
+  }
+  divide(a, b) {
+    return a / b;
+  }
+}

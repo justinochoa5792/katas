fdbaf3840a697f3c00c3dd8edbd790e5e6f9803a
Justin Ochoa <just.och92@gmail.com>
2021-11-01 19:03:42 -0400

    2 challenges

---
+++ medium/sumOfOddNum.js
@@ -0,0 +1,5 @@
+// Sum of the Odd Numbers
+// Create a function which returns the total of all odd numbers up to and including n. n will be given as an odd number.
+function addOddToN(n) {
+  return Math.pow((n + 1) / 2, 2);
+}

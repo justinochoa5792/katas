78fed747c36a8f15a4ab3024c77fbf24c0b9c30d
Justin Ochoa <just.och92@gmail.com>
2021-11-05 08:13:59 -0400

    2 challenges

---
+++ medium/DesctructureArr.js
@@ -0,0 +1,6 @@
+// Destructuring Assignment (Ignoring Values)
+// You can assign variables from arrays like this:
+
+writeyourcodehere = [1, 2, 3, 4, 5, 6, 7, 8];
+let first = writeyourcodehere[0];
+let last = writeyourcodehere[writeyourcodehere.length - 1];
---
+++ medium/TotalnumberofDigits.js
@@ -0,0 +1,6 @@
+// Find the Total Number of Digits the Given Number Has
+// Create a function that takes a number as an argument and returns the amount of digits it has.
+
+function findDigitAmount(num) {
+  return num.toString().length;
+}

432ca51f31cc059aab606795cec879b2a0170404
Justin Ochoa <just.och92@gmail.com>
2021-11-10 08:23:18 -0500

    1 challenge

---
+++ medium/isWaterBoiling.js
@@ -0,0 +1,6 @@
+// Is the Water Boiling?
+// Create a function that determines if the temp of the water is considered boiling or not. temp will be measured in fahrenheit and celsius.
+
+function isBoiling(temp) {
+  return temp >= "212F" || temp >= "100C";
+}

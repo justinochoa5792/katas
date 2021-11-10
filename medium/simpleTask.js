56d48da3da14c9ef50793197754d4cdd48bf2b09
Justin Ochoa <just.och92@gmail.com>
2021-11-09 19:51:12 -0500

    1 challenge

---
+++ medium/simpleTask.js
@@ -0,0 +1,7 @@
+// A Simple Task
+// Mubashir needs your help in a simple task.
+// Create a function that takes a number n and return its decimal part.
+
+function decimalPart(n) {
+  return (decimals = +n.toString().replace(/^[^\.]+/, "0"));
+}

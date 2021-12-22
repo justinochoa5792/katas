2caf956dabb67cbcaeeb05a553806f52eb9ccc1f
Justin Ochoa <just.och92@gmail.com>
2021-12-21 19:20:31 -0500

    1 challenge

---
+++ medium/largestNumber.js
@@ -0,0 +1,6 @@
+//  Largest Numbers
+// Create a function that takes two arguments of an array of numbers arr and a constant number n and returns the n largest numbers from the given array.
+
+function largestNumbers(n, arr) {
+  return arr.sort((a, b) => a - b).slice(arr.length - n);
+}

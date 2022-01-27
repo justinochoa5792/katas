46138caa8004421cdc11db25d7e9cd51a6d2217d
Justin Ochoa <just.och92@gmail.com>
2022-01-26 19:29:24 -0500

    2 challenges

---
+++ easy/regexXIV.js
@@ -0,0 +1,4 @@
+// RegEx XIV: Group Ranges x|y
+// Groups and ranges indicate groups and ranges of expression characters. The regular expression x|y matches either "x" or "y".
+
+const REGEXP = /blue flag|red flag/g;

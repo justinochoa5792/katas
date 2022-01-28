de445b7dd51a4bcdd4a6e07ba5d47aa589f5eb8e
Justin Ochoa <just.och92@gmail.com>
2022-01-28 18:51:02 -0500

    2 challenges

---
+++ easy/convertYear2Century.js
@@ -0,0 +1,6 @@
+// Convert Year to Century
+// Write a function that takes a year and returns its corresponding century.
+
+function centuryFromYear(year) {
+  return Math.ceil(year / 100);
+}
---
+++ easy/csmSelector.js
@@ -0,0 +1,6 @@
+// CMS Selector Based on a Given String
+// Write a function that takes an array of strings and a pattern (string) and returns the strings that contain the pattern in alphabetical order. If the pattern is an empty string, return all the strings passed in the input array.
+
+function cmsSelector(arr, str) {
+  return arr.filter((select) => select.includes(str)).sort();
+}

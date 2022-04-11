b932c4d1c2d03c809cae9bd1d62a4983aa462cf5
Justin Ochoa <just.och92@gmail.com>
2022-04-11 18:47:22 -0400

    2 challenges

---
+++ easy/alphabetSoup.js
@@ -0,0 +1,6 @@
+// Alphabet Soup
+// Create a function that takes a string and returns a string with its letters in alphabetical order.
+
+function AlphabetSoup(str) {
+  return str.split("").sort().join("");
+}
---
+++ easy/differenceBetween.js
@@ -0,0 +1,6 @@
+// Difference of Max and Min Numbers in Array
+// Create a function that takes an array and returns the difference between the biggest and smallest numbers.
+
+function diffMaxMin(arr) {
+  return Math.max(...arr) - Math.min(...arr);
+}

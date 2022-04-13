70d5ec1a5df9e0fdb8a7f49f3e18704ede2798f4
Justin Ochoa <just.och92@gmail.com>
2022-04-13 18:47:13 -0400

    1 challenge

---
+++ medium/EliminateOdd.js
@@ -0,0 +1,6 @@
+// Eliminate Odd Numbers within an Array
+// Create a function that takes an array of numbers and returns only the even values.
+
+function noOdds(arr) {
+  return arr.filter((num) => num % 2 === 0);
+}

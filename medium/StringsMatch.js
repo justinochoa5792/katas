74a368e4af89deb2ac427ea9d11900a0069b1b6a
Justin Ochoa <just.och92@gmail.com>
2022-04-25 19:05:43 -0400

    2 challenges

---
+++ medium/StringsMatch.js
@@ -0,0 +1,6 @@
+// Check if String Ending Matches Second String
+// Create a function that takes two strings and returns true if the first string ends with the second string; otherwise return false.
+
+function checkEnding(str1, str2) {
+  return str1.endsWith(str2);
+}
---
+++ medium/reverseAndCapitalize.js
@@ -0,0 +1,6 @@
+// Reverse and Capitalize
+// Create a function that takes a string of lowercase characters and returns that string reversed and in upper case.
+
+function reverseCapitalize(str) {
+  return str.split("").reverse().join("").toUpperCase();
+}

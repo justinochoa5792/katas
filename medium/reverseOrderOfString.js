56fb739e1cdf0aa47fd479e8168133a52274603e
Justin Ochoa <just.och92@gmail.com>
2022-04-27 08:13:24 -0400

    1 challenge

---
+++ medium/reverseOrderOfString.js
@@ -0,0 +1,6 @@
+// Reverse the Order of a String
+// Create a function that takes a string as its argument and returns the string in reversed order.
+
+function reverse(str) {
+  return str.split("").reverse().join("");
+}

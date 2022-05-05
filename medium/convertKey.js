eb7316595fdd2971c5339a7ea83cb7e1c2d9710b
Justin Ochoa <just.och92@gmail.com>
2022-05-04 21:24:39 -0400

    1 challenge

---
+++ medium/convertKey.js
@@ -0,0 +1,6 @@
+// Convert Key, Values in an Object to Array
+// Write a function that converts an object into an array of keys and values.
+
+function objectToArray(obj) {
+  return Object.entries(obj);
+}

eaad4a099970188a90304a444c2620677a7d705e
Justin Ochoa <just.och92@gmail.com>
2022-06-21 18:37:05 -0400

    2 challenges

---
+++ medium/RecursionToRepeat.js
@@ -0,0 +1,6 @@
+// Recursion to Repeat a String n Number of Times
+// Create a recursive function that takes two parameters and repeats the string n number of times. The first parameter txt is the string to be repeated and the second parameter is the number of times the string is to be repeated.
+
+function repetition(txt, n) {
+  return txt.repeat(n);
+}

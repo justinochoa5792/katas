6b76cc78a2040c6ebf44fff90f9caf09d4310b3a
Justin Ochoa <just.och92@gmail.com>
2022-07-08 19:18:02 -0400

    2 challenges

---
+++ medium/RegexEvenNum.js
@@ -0,0 +1,4 @@
+// Regex Series: Even Number?
+// Write a regular expression that matches only an even number. Numbers will be presented as strings.
+
+let x = /\d*[02468]$/;
---
+++ medium/firstVowel.js
@@ -0,0 +1,6 @@
+// Return the Index of the First Vowel
+// Create a function that returns the index of the first vowel in a string.
+
+function firstVowel(str) {
+  return str.search(/[aeiou]/gi);
+}

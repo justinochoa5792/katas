6bb2a224757907aed27faa6313cb47c03243ae66
Justin Ochoa <just.och92@gmail.com>
2022-09-12 19:13:25 -0400

    1 challenge

---
+++ medium/specialArray.js
@@ -0,0 +1,6 @@
+// Special Arrays
+// An array is special if every even index contains an even number and every odd index contains an odd number. Create a function that returns true if an array is special, and false otherwise.
+
+function isSpecialArray(arr) {
+  return arr.every((item, index) => item % 2 === index % 2);
+}

ccd351879f66d63d63280d487287f20727baba5d
Justin Ochoa <just.och92@gmail.com>
2022-12-20 13:38:41 -0500

    1 challenge

---
+++ medium/fizzBuzz2.js
@@ -0,0 +1,18 @@
+// The Fizz Buzz Test
+// Write a method that returns array of all the numbers from 1 to an integer argument. But for multiples of three use “Fizz” instead of the number and for the multiples of five use “Buzz”. For numbers which are multiples of both three and five use “FizzBuzz”.
+
+function fizzBuzz(number) {
+  let arr = [];
+  for (let i = 1; i <= number; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      arr.push("FizzBuzz");
+    } else if (i % 5 === 0) {
+      arr.push("Buzz");
+    } else if (i % 3 === 0) {
+      arr.push("Fizz");
+    } else {
+      arr.push(i);
+    }
+  }
+  return arr;
+}

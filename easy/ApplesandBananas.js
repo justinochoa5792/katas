4ec290fc0e7b207d8a3ef01d80a528e876ad6433
Justin Ochoa <just.och92@gmail.com>
2023-01-23 13:51:19 -0500

    1 challenge

---
+++ easy/ApplesandBananas.js
@@ -0,0 +1,6 @@
+// Apples and Bananas
+// Write a function, .vreplace() that extends the String prototype by replacing all vowels in a string with a specified vowel.
+
+String.prototype.vreplace = function (vowel) {
+  return this.replace(/[aeiou]/gi, vowel);
+};

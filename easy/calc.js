a7049ab9539d608a48c70e233a5a394aacf0e0e9
Justin Ochoa <just.och92@gmail.com>
2023-06-07 13:43:48 -0400

    1 challenge

---
+++ easy/calc.js
@@ -0,0 +1,18 @@
+// Character Code Math
+// Turn each character in a string str into its ASCII character code and join them together to create a number.
+
+function calc(str) {
+  str = str
+    .split("")
+    .map((el, i) => el.charCodeAt())
+    .join("");
+  let str1 = str
+    .split("")
+    .map((el) => +el)
+    .reduce((a, b) => a + b);
+  let str2 = str
+    .split("")
+    .map((el) => +el.replace("7", "1"))
+    .reduce((a, b) => a + b);
+  return str1 - str2;
+}

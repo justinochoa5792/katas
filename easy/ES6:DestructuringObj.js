0db9db421d15f84ac68ad0115c5cc7f141974641
Justin Ochoa <just.och92@gmail.com>
2023-06-21 13:58:20 -0400

    1 challenge

---
+++ easy/ES6:DestructuringObj.js
@@ -0,0 +1,10 @@
+// ES6: Destructuring Objects V
+// With ES6 object destructuring you make this less terse by destructuring the function parameters which reduces duplication when scaling:
+
+const str = `
+function bio({first,last}) {
+let best = user.bestFriend.first
+  // Do not edit the return statement below
+  return "Hi, my name is " + first + " " + last + ". " + best + " is my best friend."  
+ }
+`;

931d29abe1329e065dfe9c7e3cdf4a4bcd3bd297
Justin Ochoa <just.och92@gmail.com>
2023-07-20 13:32:48 -0400

    1 challenge

---
+++ medium/regexVI.js
@@ -0,0 +1,4 @@
+// RegEx: Boundary Assertions VI
+// You're needed to finish a program that lists how many tall people work in your company. All that is needed is a regular expression that will make the function work correctly.
+
+const REGEXP = /(?<=tall)/;

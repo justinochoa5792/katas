08fa08abc4d2ad341846c7f6fb6f02ba748b6a3d
Justin Ochoa <just.och92@gmail.com>
2023-08-28 14:40:56 -0400

    1 challenge

---
+++ easy/getLength.js
@@ -0,0 +1,6 @@
+// Length of a Nested Array
+// The .length property on an array will return the number of elements in the array. For example, the array below contains 2 elements:
+
+function getLength(arr) {
+  return arr.flat(4).length;
+}

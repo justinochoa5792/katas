9b8c2a31a04141c505735ebfdd125faf7089310d
Justin Ochoa <just.och92@gmail.com>
2023-09-01 14:04:31 -0400

    2 challenges

---
+++ easy/Counting sheep.js
@@ -0,0 +1,7 @@
+// Counting sheep...
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+function countSheeps(arrayOfSheep) {
+  // TODO May the force be with you
+  return arrayOfSheep.filter((el) => el === true).length;
+}

d5be443e7892839a36fd18ecba07e263de7278c4
Justin Ochoa <just.och92@gmail.com>
2023-09-11 13:43:21 -0400

    1 challenge

---
+++ medium/minMax.js
@@ -0,0 +1,6 @@
+// The highest profit wins!
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+function minMax(arr) {
+  return [Math.min(...arr), Math.max(...arr)];
+}

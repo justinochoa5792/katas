21fa461ba5332ff8240f8e3444aa5ec92a1d3caf
Justin Ochoa <just.och92@gmail.com>
2023-11-07 13:38:42 -0500

    2 challenges

---
+++ easy/booleanToString.js
@@ -0,0 +1,7 @@
+// Convert a Boolean to a String
+// Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b) {
+  //your code here
+  return b ? "true" : "false";
+}

0b4e47f55c3214a8ee7dc91ff9cd29e642a193a8
Justin Ochoa <just.och92@gmail.com>
2023-11-29 13:44:54 -0500

    2 challenges

---
+++ easy/helloWorld.js
@@ -0,0 +1,7 @@
+// Function 1 - hello world
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Write a function "greet" that returns "hello world!"
+const greet = () => {
+  return "hello world!";
+};

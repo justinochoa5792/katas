caa5a8c9639129654d7b747d6521993fca5c461b
Justin Ochoa <just.och92@gmail.com>
2023-12-01 14:18:25 -0500

    1 challenge

---
+++ easy/getEvenNumbers.js
@@ -0,0 +1,7 @@
+// JavaScript Array Filter
+// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+function getEvenNumbers(numbersArray) {
+  // filter out the odd numbers
+  return numbersArray.filter((number) => number % 2 === 0);
+}

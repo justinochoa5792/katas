a9cb10ad84859e194e421b56dd2a58a0e1ae1dfe
Justin Ochoa <just.och92@gmail.com>
2023-12-10 12:23:59 -0500

    2 challenges

---
+++ easy/basicOp.js
@@ -0,0 +1,15 @@
+// Basic Mathematical Operations
+// Your task is to create a function that does four basic mathematical operations.
+
+function basicOp(operation, value1, value2) {
+  // Code
+  if (operation === "+") {
+    return value1 + value2;
+  } else if (operation === "-") {
+    return value1 - value2;
+  } else if (operation === "*") {
+    return value1 * value2;
+  } else {
+    return value1 / value2;
+  }
+}

5f429bdb3a62ff593d1cbd461a9ba16555d4e850
Justin Ochoa <just.och92@gmail.com>
2024-01-16 13:39:17 -0500

    2 challenges

---
+++ easy/combineNames.js
@@ -0,0 +1,7 @@
+// Grasshopper - Combine strings
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Create the combineNames function here
+function combineNames(first, last) {
+  return first.concat(` ${last}`);
+}

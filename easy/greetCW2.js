cd2e78a0fdaf2e972f97b7c1088ae0f34bd04188
Justin Ochoa <just.och92@gmail.com>
2024-01-21 10:50:53 -0500

    2 challenges

---
+++ easy/greetCW2.js
@@ -0,0 +1,7 @@
+// Returning Strings
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name) {
+  //your code here
+  return `Hello, ${name} how are you doing today?`;
+}

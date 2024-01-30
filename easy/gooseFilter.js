0a5f796dd391a5df60e0449d2291e4b8e2042ea1
Justin Ochoa <just.och92@gmail.com>
2024-01-30 14:21:15 -0500

    2 challenges

---
+++ easy/gooseFilter.js
@@ -0,0 +1,10 @@
+// Filter out the geese
+// Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+// The geese are any strings in the following array, which is pre-populated in your solution:
+
+function gooseFilter(birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+  // return an array containing all of the strings in the input array except those that match strings in geese
+  return birds.filter((el) => !geese.includes(el));
+}

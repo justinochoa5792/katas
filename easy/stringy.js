a288c1810dcff59e504d1321a40e9797e6bf76fc
Justin Ochoa <just.och92@gmail.com>
2024-02-02 13:23:46 -0500

    2 challenges

---
+++ easy/stringy.js
@@ -0,0 +1,16 @@
+// Stringy Strings
+// write me a function stringy that takes a size and returns a string of alternating 1s and 0s.
+// the string should start with a 1.
+// a string with size 6 should return :'101010'.
+// with size 4 should return : '1010'.
+// with size 12 should return : '101010101010'.
+// The size will always be positive and will only use whole numbers.
+
+function stringy(size) {
+  // your code here
+  let total = [];
+  for (let i = 1; i <= size; i++) {
+    total.push(i);
+  }
+  return total.map((el) => (el % 2 === 0 ? "0" : "1")).join("");
+}

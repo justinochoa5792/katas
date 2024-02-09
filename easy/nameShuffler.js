60a693f224fa98a2eb7abe34561238ca3c94cbda
Justin Ochoa <just.och92@gmail.com>
2024-02-09 14:12:32 -0500

    2 challenges

---
+++ easy/nameShuffler.js
@@ -0,0 +1,7 @@
+// Name Shuffler
+// Write a function that returns a string in which firstname is swapped with last name.
+
+function nameShuffler(str) {
+  //Shuffle It
+  return str.split(" ").reverse().join(" ");
+}

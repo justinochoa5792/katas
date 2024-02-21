dd015fb9a0ce0ff4b7f611cd0d63dd2a397daf67
Justin Ochoa <just.och92@gmail.com>
2024-02-21 14:16:31 -0500

    1 challenge

---
+++ easy/stringClean.js
@@ -0,0 +1,7 @@
+// String cleaning
+// Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+
+function stringClean(s) {
+  // Function will return the cleaned string
+  return s.replace(/[0-9]/g, "");
+}

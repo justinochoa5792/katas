af0bebe926832da3eeca16c2b0f4f451e7df9e9f
Justin Ochoa <just.och92@gmail.com>
2024-03-05 13:52:14 -0500

    2 challenges

---
+++ easy/position.js
@@ -0,0 +1,7 @@
+// Find the position!
+// When provided with a letter, return its position in the alphabet.
+
+function position(letter) {
+  //Write your own Code!
+  return `Position of alphabet: ${letter.charCodeAt() - 96}`;
+}

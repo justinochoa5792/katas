336b1c3d38731631f2f0a759f81fc36edf58e1f6
Justin Ochoa <just.och92@gmail.com>
2024-03-14 13:38:00 -0400

    2 challenges

---
+++ easy/templateStrings.js
@@ -0,0 +1,6 @@
+//  Template Strings
+// Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+
+var templateStrings = function (noun, adjective) {
+  return `${noun} are ${adjective}`;
+};

4341e43dfa37ff1145f75ea58555b2dac74c0979
Justin Ochoa <just.och92@gmail.com>
2024-03-18 14:06:15 -0400

    1 challenge

---
+++ easy/calculateTip.js
@@ -0,0 +1,25 @@
+// Tip Calculator
+// Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
+// You need to consider the following ratings:
+// Terrible: tip 0%
+// Poor: tip 5%
+// Good: tip 10%
+// Great: tip 15%
+// Excellent: tip 20%
+// The rating is case insensitive (so "great" = "GREAT").
+
+function calculateTip(amount, rating) {
+  if (rating.toLowerCase() === "excellent") {
+    return Math.ceil(amount * 0.2);
+  } else if (rating.toLowerCase() === "great") {
+    return Math.ceil(amount * 0.15);
+  } else if (rating.toLowerCase() === "good") {
+    return Math.ceil(amount * 0.1);
+  } else if (rating.toLowerCase() === "poor") {
+    return Math.ceil(amount * 0.05);
+  } else if (rating.toLowerCase() === "terrible") {
+    return amount * 0;
+  } else {
+    return "Rating not recognised";
+  }
+}

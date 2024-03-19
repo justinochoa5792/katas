32734989059eecefe0a739d7e59d53139590f676
Justin Ochoa <just.och92@gmail.com>
2024-03-19 13:49:51 -0400

    2 challenges

---
+++ easy/calculator.js
@@ -0,0 +1,24 @@
+// simple calculator
+// Your function will accept three arguments:
+// The first and second argument should be numbers.
+// The third argument should represent a sign indicating the operation to perform on these two numbers.
+// if the variables are not numbers or the sign does not belong to the list above a message "unknown value" must be returned.
+
+function calculator(a, b, sign) {
+  switch ((a, b, sign)) {
+    case "+":
+      return a + b;
+      break;
+    case "-":
+      return a - b;
+      break;
+    case "*":
+      return a * b;
+      break;
+    case "/":
+      return a / b;
+      break;
+    default:
+      return "unknown value";
+  }
+}

3d09622a5d5089d25b3a3f02a8476ec208974f8d
Justin Ochoa <just.och92@gmail.com>
2024-03-20 13:44:45 -0400

    2 challenges

---
+++ easy/getNumberFromString.js
@@ -0,0 +1,7 @@
+// Get number from string
+// Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+function getNumberFromString(s) {
+  let regex = /[0-9]/gi;
+  return Number(s.match(regex).join(""));
+}
---
+++ easy/quote.js
@@ -0,0 +1,13 @@
+// For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre
+// If the winner is George Saint Pierre he will obviously say:
+// "I am not impressed by your performance."
+// If the winner is Conor McGregor he will most undoubtedly say:
+// "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+// Good Luck!
+
+var quote = function (fighter) {
+  // your code here
+  return fighter.toLowerCase() === "george saint pierre"
+    ? "I am not impressed by your performance."
+    : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+};

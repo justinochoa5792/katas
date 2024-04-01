14d959f7e3b2f8c5c0b9dbbbf3cc4aa3b6eedd17
Justin Ochoa <just.och92@gmail.com>
2024-04-01 13:55:35 -0400

    2 challenges

---
+++ easy/typeValidation.js
@@ -0,0 +1,7 @@
+// For Twins: 1. Types
+// Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+
+function typeValidation(variable, type) {
+  // Your code should be here ;)
+  return typeof variable === type ? true : false;
+}
---
+++ easy/yourFutureCareer.js
@@ -0,0 +1,26 @@
+// A bugs trilogy: Episode 1 - "Let Math.Random(); decide your future"
+// This code is a mess! Would you help Pac to fix the code in time?
+
+//Original
+function {
+	var : Math.random()
+		if (career <= 0.32) {
+			return = FrontEnd Developer
+		 else if (career <= 0.65) 
+			return : BackEnd Developer,
+		} else {
+			return 'Full-Stack Developer'
+		}
+yourFutureCareer();
+
+// The Fix
+function yourFutureCareer() {
+  var career = Math.random();
+  if (career <= 0.32) {
+    return "FrontEnd Developer";
+  } else if (career <= 0.65) {
+    return "BackEnd Developer";
+  } else {
+    return "Full-Stack Developer";
+  }
+}

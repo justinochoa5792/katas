50059db4845e8bfcf310b935502b0e2f0d94eb39
Justin Ochoa <just.och92@gmail.com>
2024-04-02 13:56:29 -0400

    2 challenges

---
+++ easy/power.js
@@ -0,0 +1,8 @@
+// Potenciation
+// The function takes in 2 inputs x and y, and should return x to the power of y
+// Simple, right? But you're NOT allowed to use Math.pow();
+
+function power(x, y) {
+  //SHOW ME WHAT YOU GOT!
+  return x ** y;
+}

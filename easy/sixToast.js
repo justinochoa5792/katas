d615f3d71fb0f558ae62d97d1743bab611fb91be
Justin Ochoa <just.och92@gmail.com>
2024-04-04 14:22:56 -0400

    1 challenge

---
+++ easy/sixToast.js
@@ -0,0 +1,7 @@
+// BASIC: Making Six Toast.
+// You are going to make toast fast, you think that you should make multiple pieces of toasts and once. So, you try to make 6 pieces of toast.
+
+function sixToast(num) {
+  // you code here
+  return Math.abs(6 - num)
+}

28f8aab832fb24ddd3a5049db0c6d4320cac473e
Justin Ochoa <just.och92@gmail.com>
2024-04-19 13:24:38 -0400

    1 challenge

---
+++ easy/add.js
@@ -0,0 +1,7 @@
+// Simple Comparison?
+// Write a function that will compare two values, one will be a number and one will be a string. Return true if they are the same character (regardless of their different data types) and return false if they are not.
+
+function add(a, b) {
+  // code here
+  return a == b;
+}

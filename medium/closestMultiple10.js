c287ff9e9443c639e358ab957eeef6f2f067c628
Justin Ochoa <just.och92@gmail.com>
2024-05-15 14:22:56 -0400

    1 challenge

---
+++ medium/closestMultiple10.js
@@ -0,0 +1,6 @@
+// Return the closest number multiple of 10
+// Given a number return the closest number to it that is divisible by 10.
+
+const closestMultiple10 = (num) => {
+  return Math.round(num / 10) * 10;
+};

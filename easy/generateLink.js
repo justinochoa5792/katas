fec38e418dfbd0ce0f8a3472ebfaa79344580a06
Justin Ochoa <just.och92@gmail.com>
2024-06-28 16:31:17 -0400

    1 challenge

---
+++ easy/generateLink.js
@@ -0,0 +1,6 @@
+// Generate user links
+// Your task is to create userlinks for the url, you will be given a username and must return a valid link.
+
+function generateLink(user) {
+  return `http://www.codewars.com/users/${encodeURIComponent(user)}`;
+}

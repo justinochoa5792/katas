75302858eacb233b7a5b499a994aafbc759af212
Justin Ochoa <just.och92@gmail.com>
2024-07-04 12:17:59 -0400

    1 challenge

---
+++ easy/kataExample.js
@@ -0,0 +1,8 @@
+// Kata Example Twist
+// Add the value "codewars" to the array websites 1,000 times.
+
+// add the value "codewars" to the websites array 1,000 times
+var websites = [];
+for (let i = 1; i <= 1000; i++) {
+  websites.push("codewars");
+}

211d1ce5b41c26dc62d6a6d1e48bf0c674139dd8
Justin Ochoa <just.och92@gmail.com>
2024-07-06 11:39:35 -0400

    1 challenge

---
+++ easy/ifChuckSaysSo.js
@@ -0,0 +1,6 @@
+// Chuck Norris VII - True or False? (Beginner)
+// Your task is to be more like Chuck (ha! good luck!). You must return false without ever actually using the word false...
+
+function ifChuckSaysSo() {
+  return !true;
+}

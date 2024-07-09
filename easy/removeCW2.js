970537c87d342fbc7a81833127fefeb70ba81d02
Justin Ochoa <just.och92@gmail.com>
2024-07-08 21:02:46 -0400

    1 challenge

---
+++ easy/removeCW2.js
@@ -0,0 +1,7 @@
+// Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string
+// Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+
+function remove(string) {
+  //coding and coding....
+  return string.replaceAll("!", "").concat("!");
+}

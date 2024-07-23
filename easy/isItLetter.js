f4421b061b28e7910dee35b43d4fe37aa3c932a4
Justin Ochoa <just.och92@gmail.com>
2024-07-23 14:30:02 -0400

    1 challenge

---
+++ easy/isItLetter.js
@@ -0,0 +1,6 @@
+// Is it a letter?
+// Write a function, isItLetter or is_it_letter or IsItLetter, which tells us if a given character is a letter or not.
+
+function isItLetter(character) {
+  return /[a-zA-Z]/.test(character);
+}

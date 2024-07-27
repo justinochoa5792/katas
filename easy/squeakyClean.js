a87e696433ed9e3fd2a14893e8569f91909dcefc
Justin Ochoa <just.och92@gmail.com>
2024-07-26 22:23:25 -0400

    1 challenge

---
+++ easy/squeakyClean.js
@@ -0,0 +1,8 @@
+// Squeaky Clean
+// Your squeakyClean function should accept an input array of values and return a new array with all empty strings, 0, null and undefined removed.
+
+function squeakyClean(arr) {
+  // so clever
+  // such best practice
+  return arr.filter((el) => el);
+}

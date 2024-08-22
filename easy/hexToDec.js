d567953a0b36828d5eb0982ebd430b9b1a23e74c
Justin Ochoa <just.och92@gmail.com>
2024-08-22 14:25:48 -0400

    1 challenge

---
+++ easy/hexToDec.js
@@ -0,0 +1,7 @@
+// Hex to Decimal
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+function hexToDec(hexString) {
+  //your code here
+  return parseInt(hexString, 16);
+}

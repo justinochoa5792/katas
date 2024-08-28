281c7cd00ba6ed3dca5a5477da10f17b52b43c27
Justin Ochoa <just.och92@gmail.com>
2024-08-28 17:17:22 -0400

    1 challenge

---
+++ medium/incrementString.js
@@ -0,0 +1,12 @@
+// String incrementer
+// Your job is to write a function which increments a string, to create a new string.
+// If the string already ends with a number, the number should be incremented by 1.
+// If the string does not end with a number. the number 1 should be appended to the new string.
+
+function incrementString(strng) {
+  // return incrementedString
+  let regex = /[\d]$/gi;
+  return regex.test(strng)
+    ? strng.replace(/([0-8]|\d?9+)?$/, (e) => (e ? +e + 1 : 1))
+    : strng.concat("1");
+}

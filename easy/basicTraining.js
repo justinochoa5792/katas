b56bc10561a2d2a928dfde8b3b658ef1e6a3d7d5
Justin Ochoa <just.och92@gmail.com>
2024-09-09 14:47:17 -0400

    1 challenge

---
+++ easy/basicTraining.js
@@ -0,0 +1,7 @@
+// Basic Training: Add item to an Array
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+// The websites array has already been defined for you using the following code:
+
+// add the value "codewars" to the already defined websites array
+websites.push("codewars");

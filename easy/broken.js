1c98feef9aa985fed15bf8dcde639d4a40ad60a5
Justin Ochoa <just.och92@gmail.com>
2024-10-01 13:48:46 -0400

    testing

--- easy/broken.js
+++ easy/broken.js
@@ -1,7 +1,6 @@
 // The Office III - Broken Photocopier
 // The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
 // Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
-// Given a string of binary, return the version the photocopier gives you as a string.
 
 function broken(x) {
   let char = { "1": "0", "0": "1" };

4d14d5dc377a2746e8fd9b14ae456fe847df0358
Justin Ochoa <just.och92@gmail.com>
2024-10-05 12:00:38 -0400

    2 challenge:

---
+++ easy/OrderPeople.js
@@ -0,0 +1,6 @@
+// Sort with Arrow Functions
+// Sort and Order people by their age using Arrow Functions
+
+var OrderPeople = function (people) {
+  return people.sort((a, b) => a.age - b.age); //complete this function
+};

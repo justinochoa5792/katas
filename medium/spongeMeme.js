4ca9a997e65df109baea745688a8b42e63ce3b67
Justin Ochoa <just.och92@gmail.com>
2024-11-06 13:30:26 -0500

    needed to recommit

--- medium/spongeMeme.js
+++ medium/spongeMeme.js
@@ -1,5 +1,6 @@
 // sPoNgEbOb MeMe
 // You need to create a function that converts the input into this format, with the output being the same string expect there is a pattern of uppercase and lowercase letters.
+//solution
 
 function spongeMeme(sentence) {
   return sentence

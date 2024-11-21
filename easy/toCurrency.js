8759242de7b00c8808d217a52f623b4d1c250577
Justin Ochoa <just.och92@gmail.com>
2024-11-21 14:16:40 -0500

    1 challenge

---
+++ easy/toCurrency.js
@@ -0,0 +1,7 @@
+// Converting integer to currency format
+// Integer in currency format is expressed by a string of number where every three characters are separated by comma.
+
+function toCurrency(price) {
+  //your code here
+  return price.toLocaleString();
+}

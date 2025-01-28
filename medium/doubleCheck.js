6698eceb75fc2f9b865be614f34290756289748f
Justin Ochoa <just.och92@gmail.com>
2025-01-28 14:25:14 -0500

    1 challenge

---
+++ medium/doubleCheck.js
@@ -0,0 +1,15 @@
+// Are there doubles?
+// Your job is to build a function which determines whether or not there are double characters in a string (including whitespace characters). For example aa, !! or   .
+// You want the function to return true if the string contains double characters and false if not. The test should not be case sensitive; for example both aa & aA return true.
+
+function doubleCheck(str) {
+  str = str.toLowerCase();
+
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] === str[i + 1]) {
+      return true;
+    }
+  }
+
+  return false;
+}

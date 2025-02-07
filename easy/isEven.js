720593d89aa113d117739e453385b711bf41c098
Justin Ochoa <just.och92@gmail.com>
2025-02-07 13:26:05 -0500

    1 challenge

---
+++ easy/isEven.js
@@ -0,0 +1,11 @@
+// isEven? - Bitwise Series
+// If the numbers is even return true. If it's odd, return false.
+// Oh yeah... the following symbols/commands have been disabled!
+// use of %
+// use of .even? in Ruby
+// use of mod in Python
+
+var isEven = function (n) {
+  //if n is even return true, otherwise, return false
+  return Number.isInteger(n / 2);
+};

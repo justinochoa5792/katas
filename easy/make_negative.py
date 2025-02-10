6c8f5f0af8efb20e10f195e80c48ab93f2a82f8e
Justin Ochoa <just.och92@gmail.com>
2025-02-10 13:35:08 -0500

    2 challenges

---
+++ easy/make_negative.py
@@ -0,0 +1,9 @@
+# Return Negative
+# In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+def make_negative( number ):
+    if number < 10:
+        return number
+    else:
+        return number * -1
+    
---
+++ easy/solutionCW2.py
@@ -0,0 +1,5 @@
+# Reversed Strings
+# Complete the solution so that it reverses the string passed into it.
+
+def solution(string):
+    return "".join(reversed(string))

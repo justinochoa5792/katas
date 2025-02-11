a6056d30b0b49d2759e609c1e7de757bd3309e72
Justin Ochoa <just.och92@gmail.com>
2025-02-11 13:51:59 -0500

    2 challenges

---
+++ easy/opposite.py
@@ -0,0 +1,6 @@
+# Opposite number
+# Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+def opposite(number):
+  # your solution here
+    return number * -1 

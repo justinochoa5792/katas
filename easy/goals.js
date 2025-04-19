8e589497749bf6633ca2a107322f369858035a12
Justin Ochoa <just.och92@gmail.com>
2025-04-19 10:59:59 -0400

    2 challenges

---
+++ easy/goals.js
@@ -0,0 +1,7 @@
+// Grasshopper - Messi goals function
+// Complete the function to return his total number of goals in all three leagues.
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // code goes here
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
---
+++ easy/messiGoals.js
@@ -0,0 +1,17 @@
+// Grasshopper - Messi Goals
+// Messi goal scoring statistics:
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+// Task
+// Create these three variables and store the appropriate values using the table above:
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+const laLigaGoals = 43;
+const championsLeagueGoals = 10;
+const copaDelReyGoals = 5;
+
+const totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;

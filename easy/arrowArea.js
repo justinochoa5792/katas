c011ade527a97b86364b31450ee32de68d4502d2
Justin Ochoa <just.och92@gmail.com>
2025-06-11 14:42:47 -0400

    1 challenge

---
+++ easy/arrowArea.js
@@ -0,0 +1,7 @@
+// Area of an arrow
+// An arrow is formed in a rectangle with sides a and b by joining the bottom corners to the midpoint of the top edge and the centre of the rectangle.
+
+function arrowArea(a, b) {
+  // your code
+  return (a * b) / 4;
+}

f0d62af56c0a74ac892c000b64590cfacf175147
Justin Ochoa <just.och92@gmail.com>
2025-06-14 13:08:55 -0400

    1 challenge

---
+++ easy/strangeString.js
@@ -0,0 +1,4 @@
+// Up and down, the string grows
+// However, in some special cases, the length of the transformed string may be longer than the original. Can you find a string that satisfies this criteria?
+
+const STRANGE_STRING = "ß";

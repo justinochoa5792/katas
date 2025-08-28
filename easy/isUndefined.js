26e28622aedaad4a3f5d732b22fa3ef779aa59c7
Justin Ochoa <just.och92@gmail.com>
2025-08-28 13:47:35 -0400

    1 challenge

---
+++ easy/isUndefined.js
@@ -0,0 +1,7 @@
+// Is Undefined?
+// Help Timmy remove undefined values from his array, you are modifing a callback function which he uses on each element on his array.
+
+function isUndefined(value) {
+  // Code here :)
+  return value === undefined;
+}

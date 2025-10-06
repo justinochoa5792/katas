3065c113d6e9738c1bd7aa4db9b92425e113dd87
Justin Ochoa <just.och92@gmail.com>
2025-10-06 14:28:03 -0400

    1 challenge

---
+++ easy/decode.js
@@ -0,0 +1,34 @@
+// Emily's Eccentric Encoding
+// The encoding replaces all lower case letters in a string with the opposite letter in the alphabet. In other words: a -> z, b -> y, c -> x, d -> w, etc. All other characters remain the same. The two parties have agreed on the encoding and they have generated encoded messages like:
+
+function decode(str) {
+  let chars = {
+    "a": "z",
+    "b": "y",
+    "c": "x",
+    "d": "w",
+    "e": "v",
+    "f": "u",
+    "g": "t",
+    "h": "s",
+    "i": "r",
+    "j": "q",
+    "k": "p",
+    "l": "o",
+    "m": "n",
+    "n": "m",
+    "o": "l",
+    "p": "k",
+    "q": "j",
+    "r": "i",
+    "s": "h",
+    "t": "g",
+    "u": "f",
+    "v": "e",
+    "w": "d",
+    "x": "c",
+    "y": "b",
+    "z": "a",
+  };
+  return str.replace(/[a-z]/g, (m) => chars[m]);
+}

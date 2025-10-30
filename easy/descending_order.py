072ee20ab7b6ec9d54ee8ec1e26157bd356c9c26
Justin Ochoa <just.och92@gmail.com>
2025-10-30 14:08:19 -0400

    1 challenge

---
+++ easy/descending_order.py
@@ -0,0 +1,7 @@
+# Descending Order
+# Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+def descending_order(num):
+    # Bust a move right here
+    num_str = str(num)
+    return int(''.join(sorted(num_str, reverse=True)))

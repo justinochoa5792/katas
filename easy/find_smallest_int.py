69c1f98c04827b458a2a6ebf2893eb01ed831d22
Justin Ochoa <just.och92@gmail.com>
2025-11-03 14:29:32 -0500

    1 challenge

---
+++ easy/find_smallest_int.py
@@ -0,0 +1,6 @@
+# Find the smallest integer in the array
+# Given an array of integers your solution should find the smallest integer.
+
+def find_smallest_int(arr):
+    # Code here
+    return sorted(arr)[0]

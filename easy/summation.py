43d1eb099727482bddbac5c45c9728dc4e93e3fc
Justin Ochoa <just.och92@gmail.com>
2025-11-08 11:11:10 -0500

    1 challenge

---
+++ easy/summation.py
@@ -0,0 +1,8 @@
+# Grasshopper - Summation
+# Write a program that finds the summation of every number from 1 to num (both inclusive). The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
+
+def summation(num):
+    total = 0
+    for i in range(1, num + 1):
+        total += i
+    return total

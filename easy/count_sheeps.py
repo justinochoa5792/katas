245719d5a68b80c90cc4a26605b3456abdbdae56
Justin Ochoa <just.och92@gmail.com>
2025-11-10 20:31:29 -0500

    1 challenge

---
+++ easy/count_sheeps.py
@@ -0,0 +1,6 @@
+# Counting sheep...
+# Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+def count_sheeps(sheep):
+  # TODO May the force be with you
+  return  len(list(filter(lambda x: x, sheep)))

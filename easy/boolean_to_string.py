619f36a2497c59925e279ee04f08a6a790b893ee
Justin Ochoa <just.och92@gmail.com>
2025-11-11 14:13:35 -0500

    1 challenge

---
+++ easy/boolean_to_string.py
@@ -0,0 +1,6 @@
+# Convert a Boolean to a String
+# Implement a function which convert the given boolean value into its string representation.
+
+def boolean_to_string(b):
+    #your code here
+        return str(b)

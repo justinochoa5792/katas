be2686638e5ca9d8f10bc9c95bb67e0f0d119f96
Justin Ochoa <just.och92@gmail.com>
2025-11-19 13:59:50 -0500

    1 challenge

--- easy/numberToString.php
+++ easy/numberToString.php
@@ -1,5 +1,5 @@
-Convert a Number to a String!
-We need a function that can transform a number (integer) into a string.
+<!-- Convert a Number to a String!
+We need a function that can transform a number (integer) into a string. -->
 
 <?php
   

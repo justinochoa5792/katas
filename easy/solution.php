998d7590e845d04c21f772013f63539576d77aab
Justin Ochoa <just.och92@gmail.com>
2025-11-26 07:55:53 -0500

    1 challenge

---
+++ easy/solution.php
@@ -0,0 +1,9 @@
+<!-- Reversed Strings -->
+ <!-- Complete the solution so that it reverses the string passed into it. -->
+
+ <?php
+  
+function solution(string $str): string {
+  print_r(strrev($str));
+  return strrev($str); // Your code here
+}

43443870bb8a728acf967204f66543aab9906905
Justin Ochoa <just.och92@gmail.com>
2025-12-11 15:09:26 -0500

    1 challenge

---
+++ easy/no_space.py
@@ -0,0 +1,6 @@
+# Remove String Spaces
+# Write a function that removes the spaces from the string, then return the resultant string.
+
+def no_space(x):
+    #your code here
+    return x.replace(" ", "")

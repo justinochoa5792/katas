ff3fc8bc9dfe8b94f642153d20755b49e46c3793
Justin Ochoa <just.och92@gmail.com>
2025-12-16 13:59:35 -0500

    1 challenge

---
+++ easy/greet.py
@@ -0,0 +1,6 @@
+# Returning Strings
+# Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
+
+def greet(name):
+    #Good Luck (like you need it)
+    return "Hello, "+ name + " how are you doing today?"

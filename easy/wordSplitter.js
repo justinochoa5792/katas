64c918a9bcd309da07379ea7fae4c0462ad9df20
Justin Ochoa <just.och92@gmail.com>
2026-01-13 15:22:49 -0500

    1 challenge

---
+++ easy/wordSplitter.js
@@ -0,0 +1,7 @@
+// Strange Strings Parser!
+// Here you have a connected to a socket, and the data looks very strange. It seems to be separated by a random special character! Oh No! We need your help to find the special character, parse the data, and return it translated! There isn't much time, hurry we need your help!
+
+function wordSplitter(string) {
+  //your code here
+  return string.split(/[^a-z0-9-.]/gi);
+}

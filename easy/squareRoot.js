5a5a699b55c085f742b00d8e5611f6d2cbec0817
Justin Ochoa <just.och92@gmail.com>
2026-02-05 14:58:16 -0500

    1 challenge

---
+++ easy/squareRoot.js
@@ -0,0 +1,7 @@
+// Find the Square Root
+// Create a Function that takes one parameter and returns its square root rounded to 5 decimal places. You are not allowed to use the Math.sqrt() or the Math.pow() methods.
+
+function squareRoot(x) {
+  //Good luck!
+  return Number((x ** 0.5).toFixed(5));
+}

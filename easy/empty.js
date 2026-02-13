5bbda0377fd1660244e30bd41b5a70cd319f98b0
Justin Ochoa <just.och92@gmail.com>
2026-02-13 14:44:24 -0500

    1 challenge

---
+++ easy/empty.js
@@ -0,0 +1,7 @@
+// Empty that array!
+// Today's task is to write a function empty(), which take an array as argument. This function should return the same array, but empty.
+
+function empty(array) {
+  array.length = 0;
+  return array;
+}

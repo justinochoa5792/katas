8bb360b5941f6f07ce7a7b5e398e878f6cfc77a6
Justin Ochoa <just.och92@gmail.com>
2026-03-03 14:40:33 -0500

    1 challenge

---
+++ easy/truthyFalsy.js
@@ -0,0 +1,5 @@
+// Truthy and Falsy
+// You are given two empty arrays (truthy and falsy) and you have to fill this array with at least 5 elements in each which will evaluate to true or false accordingly.
+
+const truthy = [1, 2, 3, 4, 5];
+const falsy = [false, null, undefined, NaN, ""];

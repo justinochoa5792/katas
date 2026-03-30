d227a6cc95efeb4fcc744e2a70cac9d59a4192a4
Justin Ochoa <just.och92@gmail.com>
2026-03-30 15:32:59 -0400

    1 challenge

---
+++ easy/digitAll.js
@@ -0,0 +1,11 @@
+// DigitAll
+// In this easy kata your function has to take a string as input and return a string with everything removed (whitespaces included) but the digits. As you may have guessed empty strings are to be returned as they are & if the input string contains no digits then the output will be an empty string.By the way , you have to watch out for non-string inputs too.Return 'Invalid input !' for them.
+
+function digitAll(x) {
+  // Here you go :
+  return typeof x !== "string"
+    ? "Invalid input !"
+    : !x.match(/[\d]/gi)
+      ? ""
+      : x.match(/[\d]/gi).join("");
+}

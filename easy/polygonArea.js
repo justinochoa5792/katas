61ac8130c6fe970135525a5f97fa9a86820a72d3
Justin Ochoa <just.och92@gmail.com>
2026-04-06 15:14:30 -0400

    1 challenge

---
+++ easy/polygonArea.js
@@ -0,0 +1,6 @@
+// Area of a Polygon (Easy)
+// Write the function polygonArea(A,B,C,D) that finds the area of polygons of this type:
+
+function polygonArea(A, B, C, D) {
+  return ((A + C) / 2) * B;
+}

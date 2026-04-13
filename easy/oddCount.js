853ed92176d32049715f47ba0c117ecaa0448a8a
Justin Ochoa <just.och92@gmail.com>
2026-04-13 15:13:34 -0400

    1 challenge

---
+++ easy/oddCount.js
@@ -0,0 +1,7 @@
+// Count Odd Numbers below n
+// Given a number n, return the number of positive odd numbers below n, EASY!
+
+function oddCount(n) {
+  // your code here
+  return Math.floor(n / 2);
+}

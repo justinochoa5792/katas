3ef79285cb12f25b6eb91fe2868eb472d4a70c2f
Justin Ochoa <just.och92@gmail.com>
2026-04-27 15:41:48 -0400

    1 challenge

---
+++ easy/firstToLast.js
@@ -0,0 +1,8 @@
+// Training JS #17: Methods of String object--indexOf(), lastIndexOf() and search()
+// Coding in function firstToLast, function accept 2 parameters:str and c. str is a string. c is a char.
+// Please return the gap between the first position of c and the last position of c.
+// If there are a lot of c in the str, should return a positive integer; If there is only one c in str, should return 0; If there is no c in the str, should return -1. Retrieval should not ignored the case.
+
+function firstToLast(str,c){
+  return str.lastIndexOf(c) == -1 || str.indexOf(c) == -1 ? -1 : str.lastIndexOf(c) - str.indexOf(c) 
+}

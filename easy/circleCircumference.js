b28928810fdf019d3534c54279fa6ad4990beb65
Justin Ochoa <just.och92@gmail.com>
2026-04-29 14:28:44 -0400

    1 challenge

---
+++ easy/circleCircumference.js
@@ -0,0 +1,6 @@
+// Geometry Basics: Circle Circumference in 2D
+// Point objects have x, y attributes. Circle objects have center which is a Point, and radius, which is a number.
+
+function circleCircumference(circle) {
+  return 2 * Math.PI * circle.radius;
+}

40413e65f1494ba57032adae9a092f6e67ee930e
Justin Ochoa <just.och92@gmail.com>
2026-05-05 14:44:54 -0400

    1 challenge

---
+++ easy/Labrador.js
@@ -0,0 +1,15 @@
+// SpeedCode #3 × Fun with ES6 Classes #5 - Dogs and Classes
+// Shorten it so that it meets the strict character count requirements for this Kata.
+
+class Labrador {
+  constructor(n, a, g, m) {
+    this.name = n;
+    this.age = a;
+    this.gender = g;
+    this.species = "Labrador";
+    this.legs = 4;
+    this.size = "Large";
+    this.master = m;
+    this.loyal = true;
+  }
+}

feab99253620a19d163eb9cbdc6363c6832cae87
Justin Ochoa <just.och92@gmail.com>
2026-05-27 14:20:05 -0400

    1 challenge

---
+++ easy/isNegative.js
@@ -0,0 +1,6 @@
+// There exist two zeroes: +0 (or just 0) and -0.
+// Write a function that returns true if the input number is -0 and false otherwise (True and False for Python).
+
+function isNegativeZero(n) {
+  return n === 0 && 1 / n === -Infinity;
+}

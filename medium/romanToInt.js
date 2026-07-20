682dbe04cfe48c8b2f3db282fa61b0bed01a08bf
Justin Ochoa <just.och92@gmail.com>
2026-07-20 15:09:29 -0400

    1 challenge

---
+++ medium/romanToInt.js
@@ -0,0 +1,32 @@
+// 13. Roman to Integer
+// Given a roman numeral, convert it to an integer.
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  const values = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const current = values[s[i]];
+    const next = values[s[i + 1]];
+
+    if (current < next) {
+      total -= current;
+    } else {
+      total += current;
+    }
+  }
+
+  return total;
+};

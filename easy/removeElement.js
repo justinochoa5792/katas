92d6dc320da0c929183e86c39b7b8dd475205c8c
Justin Ochoa <just.och92@gmail.com>
2026-07-22 14:39:43 -0400

    1 challenge

---
+++ easy/removeElement.js
@@ -0,0 +1,20 @@
+// 27. Remove Element
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let k = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+};

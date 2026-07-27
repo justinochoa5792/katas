692d83ad7d58618ed51c2ff4dac82d2b922eec8e
Justin Ochoa <just.och92@gmail.com>
2026-07-27 14:10:38 -0400

    1 challenge

---
+++ easy/evenCheck.js
@@ -0,0 +1,4 @@
+// [Code Golf] Even Check
+// Create a function that accepts a number n, 1 <= n <= 1000, as a parameter and returns true if the number is even and false if the number is odd.
+
+evenCheck = (n) => !(n & 1);

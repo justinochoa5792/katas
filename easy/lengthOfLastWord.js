e0976e6e4ea60ed949b135ed40a457a45c08011a
Justin Ochoa <just.och92@gmail.com>
2026-07-28 14:12:54 -0400

    1 challenge

---
+++ easy/lengthOfLastWord.js
@@ -0,0 +1,11 @@
+// 58. Length of Last Word
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+// A word is a maximal substring consisting of non-space characters only.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  return s.trim().split(" ")[s.trim().split(" ").length - 1].length;
+};

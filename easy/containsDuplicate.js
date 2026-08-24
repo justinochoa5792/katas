2a343b646035bde97e5a279bc0c81ca497b0e20f
Justin Ochoa <just.och92@gmail.com>
2026-08-24 15:28:02 -0400

    1 challenge

---
+++ easy/containsDuplicate.js
@@ -0,0 +1,11 @@
+// 217. Contains Duplicate
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  let dupes = Array.from(new Set(nums));
+  return dupes.length !== nums.length;
+};

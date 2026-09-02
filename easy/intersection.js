f85f55815d6df02e205b0b19ecb15270e9d07b1f
Justin Ochoa <just.och92@gmail.com>
2026-09-02 14:59:00 -0400

    1 challenge

---
+++ easy/intersection.js
@@ -0,0 +1,11 @@
+// 349. Intersection of Two Arrays
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+  return [...new Set(nums1.filter((el) => nums2.includes(el)))];
+};

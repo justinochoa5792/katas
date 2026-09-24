569ba2c24590818c12d2f3f8e144d4f1092b7b60
Justin Ochoa <just.och92@gmail.com>
2026-09-24 14:38:25 -0400

    1 challenge

---
+++ easy/myPow.js
@@ -0,0 +1,11 @@
+// 50. Pow(x, n)
+// Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function (x, n) {
+  return x ** n;
+};
